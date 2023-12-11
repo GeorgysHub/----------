de4deb9d9e26b9c987ab923bec6a7d9fe36bcd14
Georgy Balashov <gosha.balashov.1002@gmail.com>
2023-12-11 19:18:31 +0400

    Code has been updated

--- originalCode.js
+++ originalCode.js
@@ -1,7 +1,12 @@
 // Оригинальный код
 function helloWorld() {
     console.log('ААААА КРОКОДИЛ В ВАНОЙ!');
+    let n = 10
+    for(let i = 0;i<n;i++){
+        console.log(i);
+    }
 }
+
 
 // Вызываем функцию из оригинального кода
 helloWorld();
